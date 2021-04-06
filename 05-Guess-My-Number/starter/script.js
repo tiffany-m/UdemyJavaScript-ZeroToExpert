2f3fca904ac2873d0045fa7e0238135c645c29a9
Tiffany <tiffany_mollica@yahoo.com>
2021-04-06 11:58:46 -0400

    added game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,45 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent); 
-document.querySelector('.message').textContent = 'Correct Number'
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent); 
+// document.querySelector('.message').textContent = 'Correct Number'
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); 
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value); 
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if(!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = 'Number is too High';
+      score--;
+      document.querySelector('.score').textContent = score; 
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Number is too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+})
