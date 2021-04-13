e05a24f0ad8e79283cc5a72e445842e77a65a577
Tiffany <tiffany_mollica@yahoo.com>
2021-04-13 13:15:04 -0400

    practiced DOM set bg to change when game is won

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,10 +19,17 @@ document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // when there is no input
   if(!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+  // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = "30rem";
+  // when guess is too high
   } else if (guess > secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = 'Number is too High';
@@ -32,6 +39,7 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.message').textContent = 'You Lost the Game';
       document.querySelector('.score').textContent = 0;
     }
+  // when guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Number is too Low';
