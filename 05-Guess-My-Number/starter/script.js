bac580a0569a90392a305c51b50ce248fec43835
Tiffany <tiffmollica@gmail.com>
2021-04-22 16:45:41 -0400

    added funtion to again button for restarting game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,10 +10,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value); 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-document.querySelector('.number').textContent = secretNumber
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,9 +24,8 @@ document.querySelector('.check').addEventListener('click', function() {
   // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber; 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = "30rem";
   // when guess is too high
   } else if (guess > secretNumber) {
@@ -50,4 +48,17 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.score').textContent = 0;
     }
   }
+})
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 })
